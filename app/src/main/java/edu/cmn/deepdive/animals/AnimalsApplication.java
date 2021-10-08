8b222edd8534860044844da81b45596470646d07
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-08 14:06:48 -0400

    Added listener to spnner: displays image.

---
+++ app/src/main/java/edu/cmn/deepdive/animals/AnimalsApplication.java
@@ -0,0 +1,17 @@
+package edu.cmn.deepdive.animals;
+
+import android.app.Application;
+import com.squareup.picasso.Picasso;
+
+public class AnimalsApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Picasso.setSingletonInstance(
+        new Picasso.Builder(this)
+            .loggingEnabled(BuildConfig.DEBUG)
+            .build()
+    );
+  }
+}
